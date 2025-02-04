345bda224b774f11ca36f730f8cebba67e1a069a
shishir gupta <shishir.gupta@aptaracorp.com>
2025-02-04 09:11:21 +0530

    chapter 8 completed

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -1,7 +1,9 @@
 import { generateYAxis } from '@/app/lib/utils';
 import { CalendarIcon } from '@heroicons/react/24/outline';
 import { lusitana } from '@/app/ui/fonts';
-import { Revenue } from '@/app/lib/definitions';
+
+import { fetchRevenue } from '@/app/lib/data';
+
 
 // This component is representational only.
 // For data visualization UI, check out:
@@ -9,13 +11,9 @@ import { Revenue } from '@/app/lib/definitions';
 // https://www.chartjs.org/
 // https://airbnb.io/visx/
 
-export default async function RevenueChart({
-  revenue,
-}: {
-  revenue: Revenue[];
-}) {
+export default async function RevenueChart() {
   const chartHeight = 350;
-  // NOTE: Uncomment this code in Chapter 7
+  const revenue = await fetchRevenue(); 
 
    const { yAxisLabels, topLabel } = generateYAxis(revenue);
 
